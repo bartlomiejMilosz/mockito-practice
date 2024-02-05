3515e7934258c0b3c2da27f9403da19e8d7e27e3
Bart Milo <bartlomiej.milosz@gmail.com>
2024-02-05 20:30:11 +0100

    Initial commit

---
+++ src/test/java/io/github/bartlomiejmilosz/happyhotel/booking/TestSanityCheck.java
@@ -0,0 +1,13 @@
+package io.github.bartlomiejmilosz.happyhotel.booking;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestSanityCheck {
+
+    @Test
+    void test() {
+        //...
+    }
+}
